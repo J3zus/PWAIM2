88fc5bb15aeaf221d0aec27c1813d2d77fafa8f1
Jesus Fuentes <blas.jes17@gmail.com>
2022-01-07 15:08:28 -0600

    vN0.4

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ mysql = MySQL()
 app.config['MYSQL_DATABASE_HOST'] = '127.0.0.1'
 app.config['MYSQL_DATABASE_USER'] = 'root'
 app.config['MYSQL_DATABASE_PASSWORD'] = 'root'
-app.config['MYSQL_DATABASE_DB'] = 'clasepython'
+app.config['MYSQL_DATABASE_DB'] = 'si'
 mysql.init_app(app)
 
 CARPETA = os.path.join('uploads')
@@ -19,75 +19,212 @@ app.config['CARPETA']= CARPETA
 
 @app.route('/')
 def index():
-    sql = "SELECT * FROM `empleados`;"
+    return render_template('index.html')
+
+
+@app.route('/indexme')
+def indexme():
+    return render_template('Menu.html')
+
+
+@app.route('/indexbe')
+def indexbe():
+
+    sql = "SELECT * FROM `bebidas`;"
     conn = mysql.connect()
     cursor = conn.cursor()
     cursor.execute(sql)
 
-    empleados = cursor.fetchall()
+    bebidas = cursor.fetchall()
 
     conn.commit()
+    return render_template('Bebidas/index.html', bebidas=bebidas)
 
-    return render_template('empleados/index.html', empleados=empleados)
+@app.route('/indexbo')
+def indexbo():
 
-@app.route('/create')
-def create():
-    return render_template('empleados/create.html')
+    sql2 = "SELECT * FROM `botana`;"
+    conn2 = mysql.connect()
+    cursor2 = conn2.cursor()
+    cursor2.execute(sql2)
+
+    botanas = cursor2.fetchall()
+
+    conn2.commit()
+
+    return render_template('botana/index.html', botanas=botanas)
+
+@app.route('/indexre')
+def indexre():
+
+    sql3 = "SELECT * FROM `reservaciones`;"
+    conn3 = mysql.connect()
+    cursor3 = conn3.cursor()
+    cursor3.execute(sql3)
+
+    reservaciones = cursor3.fetchall()
+
+    conn3.commit()
+    return render_template('Reservaciones/index.html', reservaciones=reservaciones)
+
+
+
+
+@app.route('/createbebidas')
+def createbebidas():
+    return render_template('Bebidas/create.html')
+
+@app.route('/createbotanas')
+def createbotanas():
+    return render_template('botana/create.html')
+
+@app.route('/createreservacion')
+def createreservacion():
+    return render_template('Reservaciones/create.html')
 
 @app.route('/store', methods=['POST'])
 def storage():
     _nombre = request.form['txtNombre']
-    _correo = request.form['txtCorreo']
-    _foto = request.files['txtFoto']
+    _descripcion = request.form['txtdescripcion']
+    _tipo = request.form['listtipo']
+    _cantidad = request.form['txtcantidad']
+    _precio = request.form['txtprecio']
+    _imagen = request.files['txtFoto']
 
     now = datetime.now()
     tiempo = now.strftime("%Y%H%M%S")
 
-    if _foto.filename != '':
-        nuevoNombre = tiempo + _foto.filename
-        _foto.save("uploads/"+nuevoNombre)
+    if _imagen.filename != '':
+        nuevoNombre = tiempo + _imagen.filename
+        _imagen.save("uploads/"+nuevoNombre)
 
-    sql = "insert into `empleados` (`id`, `nombre`, `correo`, `foto`) VALUES(NULL, %s, %s, %s);"
-    datos = (_nombre, _correo, nuevoNombre)
+    sql = "insert into `bebidas` (`Id`, `Nombre`, `Descripcion`, `Tipo`, `Cantidad`, `Precio`, `Imagen`) VALUES(NULL, %s, %s, %s, %s, %s, %s);"
+    datos = (_nombre, _descripcion,_tipo,_cantidad,_precio, nuevoNombre)
     conn = mysql.connect()
     cursor = conn.cursor()
     cursor.execute(sql, datos)
     conn.commit()
-    return redirect('/')
+    return redirect('/indexbe')
 
-@app.route('/destroy/<int:id>')
-def destroy(id):
+@app.route('/store2', methods=['POST'])
+def storage2():
+    _nombre = request.form['txtNombre']
+    _descripcion = request.form['txtdescripcion']
+    _precio = request.form['txtprecio']
+    _cantidad = request.form['txtcantidad']
+    _imagen = request.files['txtFoto']
+
+    now = datetime.now()
+    tiempo = now.strftime("%Y%H%M%S")
+
+    if _imagen.filename != '':
+        nuevoNombre = tiempo + _imagen.filename
+        _imagen.save("uploads/"+nuevoNombre)
+
+    sql = "insert into `botana` (`Id`, `Nombre`, `Descripcion`, `Cantidad`, `Precio`, `Imagen`) VALUES(NULL, %s, %s, %s, %s, %s);"
+    datos = (_nombre, _descripcion, _cantidad, _precio, nuevoNombre)
+    conn = mysql.connect()
+    cursor = conn.cursor()
+    cursor.execute(sql, datos)
+    conn.commit()
+    return redirect('/indexbo')
+
+@app.route('/store3', methods=['POST'])
+def storage3():
+    _nombre = request.form['txtNombre']
+    _mesa = request.form['txtMesa']
+    _precio = request.form['txtprecio']
+    _fecha = request.form['txtFecha']
+    _Hora = request.form['txtHora']
+
+    sql = "insert into `reservaciones` (`Id`, `Nombre`, `Mesa`, `Fecha`, `Hora`, `Precio`) VALUES(NULL, %s, %s, %s, %s, %s);"
+    datos = (_nombre, _mesa,_fecha, _Hora, _precio)
+    conn = mysql.connect()
+    cursor = conn.cursor()
+    cursor.execute(sql, datos)
+    conn.commit()
+    return redirect('/indexre')
+
+@app.route('/destroybebidas/<int:id>')
+def destroybebidas(id):
     conn = mysql.connect()
     cursor = conn.cursor()
 
-    cursor.execute("SELECT foto FROM empleados WHERE id=%s", (id))
+    cursor.execute("SELECT Imagen FROM bebidas WHERE Id=%s", (id))
     fila = cursor.fetchall()
     os.remove(os.path.join(app.config['CARPETA'], fila[0][0]))
 
-    cursor.execute("DELETE FROM empleados WHERE id=%s", (id))
+    cursor.execute("DELETE FROM bebidas WHERE id=%s", (id))
     conn.commit()
-    return redirect('/')
+    return redirect('/indexbe')
 
-@app.route('/edit/<int:id>')
-def edit(id):
+@app.route('/destroybotana/<int:id>')
+def destroybotana(id):
     conn = mysql.connect()
     cursor = conn.cursor()
 
-    cursor.execute("SELECT * FROM empleados WHERE id=%s", (id))
-    empleados = cursor.fetchall()
+    cursor.execute("SELECT Imagen FROM botana WHERE Id=%s", (id))
+    fila = cursor.fetchall()
+    os.remove(os.path.join(app.config['CARPETA'], fila[0][0]))
+
+    cursor.execute("DELETE FROM botana WHERE Id=%s", (id))
     conn.commit()
-    return render_template('empleados/edit.html', empleados=empleados)
+    return redirect('/indexbo')
+
+
+@app.route('/destroyreservacion/<int:id>')
+def destroyreservacion(id):
+    conn = mysql.connect()
+    cursor = conn.cursor()
+
+    cursor.execute("DELETE FROM reservaciones WHERE Id=%s", (id))
+    conn.commit()
+    return redirect('/indexre')
+
+@app.route('/editbebidas/<int:id>')
+def editbebidas(id):
+    conn = mysql.connect()
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT * FROM bebidas WHERE id=%s", (id))
+    bebidas = cursor.fetchall()
+    conn.commit()
+    return render_template('bebidas/edit.html', bebidas=bebidas)
+
+@app.route('/editbotana/<int:id>')
+def editbotana(id):
+    conn = mysql.connect()
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT * FROM botana WHERE Id=%s", (id))
+    botanas = cursor.fetchall()
+    conn.commit()
+    return render_template('botana/edit.html', botanas=botanas)
+
+@app.route('/editreservaciones/<int:id>')
+def editreservaciones(id):
+    conn = mysql.connect()
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT * FROM reservaciones WHERE Id=%s", (id))
+    reservaciones = cursor.fetchall()
+    conn.commit()
+    return render_template('Reservaciones/edit.html', reservaciones=reservaciones)
 
 @app.route('/update', methods=['POST'])
 def update():
     _nombre = request.form['txtNombre']
-    _correo = request.form['txtCorreo']
+    _descripcion = request.form['txtdescripcion']
+    _tipo = request.form['listtipo']
+    _cantidad = request.form['txtcantidad']
+    _precio = request.form['txtprecio']
+
     _foto = request.files['txtFoto']
     id = request.form['txtID']
 
-    sql = "UPDATE empleados SET nombre=%s, correo=%s WHERE id=%s;"
+    sql = "UPDATE bebidas SET Nombre=%s, Descripcion=%s, Tipo=%s, Cantidad=%s, Precio=%s WHERE Id=%s;"
 
-    datos = (_nombre, _correo, id)
+    datos = (_nombre, _descripcion, _tipo, _cantidad, _precio, id)
 
     conn = mysql.connect()
     cursor = conn.cursor()
@@ -100,14 +237,69 @@ def update():
     if _foto.filename !='':
         nuevoNombre = hora + _foto.filename
         _foto.save('uploads/'+nuevoNombre)
-        cursor.execute("SELECT foto FROM empleados WHERE id=%s", id)
+        cursor.execute("SELECT Imagen FROM bebidas WHERE Id=%s", id)
         fila = cursor.fetchall()
 
         os.remove(os.path.join(app.config['CARPETA'], fila[0][0]))
-        cursor.execute("UPDATE empleados SET foto=%s WHERE id=%s", (nuevoNombre, id))
+        cursor.execute("UPDATE bebidas SET Imagen=%s WHERE Id=%s", (nuevoNombre, id))
         conn.commit()
 
-    return redirect('/')
+    return redirect('/indexbe')
+
+@app.route('/updatebotana', methods=['POST'])
+def updatebotana():
+    _nombre = request.form['txtNombre']
+    _descripcion = request.form['txtdescripcion']
+    _precio = request.form['txtprecio']
+
+    _foto = request.files['txtFoto']
+    id = request.form['txtID']
+
+    sql = "UPDATE botana SET Nombre=%s, Precio=%s, Descripcion=%s WHERE Id=%s;"
+
+    datos = (_nombre, _precio, _descripcion, id)
+
+    conn = mysql.connect()
+    cursor = conn.cursor()
+    cursor.execute(sql, datos)
+    conn.commit()
+
+    now = datetime.now()
+    hora = now.strftime("%Y%H%M%S")
+
+    if _foto.filename !='':
+        nuevoNombre = hora + _foto.filename
+        _foto.save('uploads/'+nuevoNombre)
+        cursor.execute("SELECT Imagen FROM botana WHERE Id=%s", id)
+        fila = cursor.fetchall()
+
+        os.remove(os.path.join(app.config['CARPETA'], fila[0][0]))
+        cursor.execute("UPDATE botana SET Imagen=%s WHERE Id=%s", (nuevoNombre, id))
+        conn.commit()
+
+    return redirect('/indexbo')
+
+
+@app.route('/updatereservacion', methods=['POST'])
+def updatereservacion():
+    _nombre = request.form['txtNombre']
+    _mesa = request.form['txtMesa']
+    _Fecha = request.form['txtFecha']
+    _Hora = request.form['txtHora']
+    _precio = request.form['txtprecio']
+    id = request.form['txtID']
+
+    sql = "UPDATE reservaciones SET Nombre=%s, Mesa=%s, Fecha=%s, Hora=%s, Precio=%s WHERE Id=%s;"
+
+    datos = (_nombre, _mesa, _Fecha, _Hora, _precio,id)
+
+    conn = mysql.connect()
+    cursor = conn.cursor()
+    cursor.execute(sql, datos)
+    conn.commit()
+
+    return redirect('/indexre')
+
 
 @app.route('/uploads/<nombreFoto>')
 def uploads(nombreFoto):
